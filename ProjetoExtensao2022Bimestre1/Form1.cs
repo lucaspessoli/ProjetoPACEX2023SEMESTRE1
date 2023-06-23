32c7dbc46cf92b2382835025fc334db0742ed6c5
Lucas Camacho <pessolilucas@gmail.com>
2023-06-23 13:29:37 -0300

    IMPLEMENT esqueleto back-end

--- ProjetoExtensao2022Bimestre1/Form1.cs
+++ ProjetoExtensao2022Bimestre1/Form1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace ProjetoExtensao2022Bimestre1
 {
@@ -15,6 +9,42 @@ namespace ProjetoExtensao2022Bimestre1
         public Form1()
         {
             InitializeComponent();
+            comboBox1.Items.Add("Opção 1");
+            comboBox1.Items.Add("Opção 2");
+            comboBox1.Items.Add("Opção 3");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String db = "Server=localhost;Port=5432;Database=ProjetoPACEX;" +
+                "User Id=postgres;Password=123;";
+            NpgsqlConnection conexao = new NpgsqlConnection(db);
+            String comando = "INSERT INTO Avaliacao (ra_pessoa, nota, comentario) VALUES (002223, 5, 'ótima aula');";
+            try
+            {
+                NpgsqlCommand command = new NpgsqlCommand(comando, conexao);
+                conexao.Open();
+                command.ExecuteNonQuery();
+                conexao.Close();
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Um erro ocorreu...");
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
